3f1e88d8c65d50d685d426bf5631e15ed95df731
tomnetgeneraltradingplc <tomnetgeneraltradingplc@gmail.com>
2019-06-01 18:43:25 -0500

    String Length Test

---
+++ src/Test/StringLength3TestTest.java
@@ -0,0 +1,22 @@
+package Test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by Thomas Tibebu,
+ * Date May 20, 2019
+ **/
+class StringLength3TestTest {
+
+    @Test
+    void isStringLengthIs3() {
+        int str1 = "tom".length();
+        int str2 = "Run".length();
+        int str3 = "May".length();
+        assertEquals(3, str1);
+        assertEquals(3, str2);
+        assertEquals(3, str3);
+    }
+}
